0266f85616b6c7301b596142d3d989a4147e5871
Aurélien Brabant <abrabant@student.42.fr>
2021-08-23 00:41:15 +0200

    Exclude some characters from text_to_css_id and also document it

--- lib/text_to_css_id.ts
+++ lib/text_to_css_id.ts
@@ -1,4 +1,7 @@
 export const textToCSSId = (text: string): string =>
 {
-	return text.toLowerCase().replace(/"|'|`|\!|\?|,|;|\(|\)|/g, '').replace(/\s/g, '-').replace(/(\-)\1+/g, '-').replace(/\-+$/g, '');
+	return text.toLowerCase().replace(/"|'|`|\!|\?|,|;|\(|\)|:/g, '') // remove a list of unwanted characters
+		.replace(/\s/g, '-') // replace whitespaces by dashes
+		.replace(/(\-)\1+/g, '-') // replace consecutive dashes by only one
+		.replace(/\-+$/g, ''); // remove trailing dashes
 }
