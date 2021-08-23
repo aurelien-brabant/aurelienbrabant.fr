c39cc2f19c694f6eecdb51acd5cbbe3f942d6333
Aurélien Brabant <abrabant@student.42.fr>
2021-08-23 13:29:47 +0200

    Add wonderful progress bar (nprogress)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
+import React from 'react';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {Layout} from '../components/layout/Layout'
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import Head from 'next/head';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <React.Fragment>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </React.Fragment>
   )
 }
 
