64d3029a68ff1879073d07f275e60346abc90617
abrabant <abrabant@student.42.fr>
2021-09-30 21:45:10 +0200

    Add simple contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { NextPage } from "next";
+import { Container } from "../components/container/container";
+
+import styles from "../styles/Contact.module.css";
+
+const ContactPage: NextPage = () => {
+  return (
+    <Container limitedWidth={false} className={styles.contactWrapper}
+        fillPageHeight={true}
+    >
+      <Container className={styles.contactContainer}
+      >
+        <h1>Contact</h1>
+        <p>Thank you for your interest in getting in touch!</p>
+
+        <p>
+          Whether it's to discuss about stuff I've written on my blog or to work
+          with me, I'm happy to get your email.
+        </p>
+
+        <p>
+          <b>
+            The best way to reach me is by email at
+            <a href="mailto:hi@aurelienbrabant.fr" className={styles.email}>
+              {" "}
+              hi@aurelienbrabant.fr.
+            </a>
+          </b>
+        </p>
+
+        <p>
+          You can also visit my {" "}
+          <a href="https://github.com/aurelien-brabant" target="_blank">GitHub</a>,{" "}
+          <a href="https://www.linkedin.com/in/aurelien-brabant" target="_blank">LinkedIn</a>,{" "}
+          <a href="https://twitter.com/aurelienb42" target="_blank">Twitter</a>,{" "}
+          or even <a href="https://www.youtube.com/channel/UC9JjIHlcttAz6QJTVjVxsdg" target="_blank">YouTube</a> channel.
+        </p>
+      </Container>
+    </Container>
+  );
+};
+
+export default ContactPage;
